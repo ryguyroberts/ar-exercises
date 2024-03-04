397794d82c2c284e952d4ad27c8399ea406b815e
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-04 12:50:51 -0800

    Add exercise 7 and validation in store and employee

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+store = Store.new(name: store_name)
+
+if store.save
+  puts "Store '#{store.name}' created successfully!"
+else
+  puts "Failed to create store due to the following errors:"
+  store.errors.full_messages.each do |error_message|
+    puts error_message
+  end
+end
